661ca54ae612ff143faae5aa0242c80faf7dfd38
andrzej-romankiewicz <romankirwiczxd@gmail.com>
2026-04-15 19:21:08 +0200

    main.py

--- main.py
+++ main.py
@@ -9,14 +9,15 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
-    # Przykładowe dane skinów
     skins_data = [
         {"name": "AK-47 | Slate (FT)", "price": 12.50},
         {"name": "M4A1-S | Mecha Industries", "price": 25.80},
         {"name": "AWP | Atheris", "price": 9.20}
     ]
     
-    return templates.TemplateResponse("index.html", {
-        "request": request, 
-        "skins": skins_data
-    })
+    # TUTAJ JEST ZMIANA: Dodajemy request=, name= i context=
+    return templates.TemplateResponse(
+        request=request, 
+        name="index.html", 
+        context={"skins": skins_data}
+    )
